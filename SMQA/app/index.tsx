5299ec81e6e9a627b056900b4bcdad515d691848
gutox2001 <aroldoaugusto10@gmail.com>
2025-01-07 00:22:15 -0300

    :sparkles: feat: Pequenas atualizações

--- SMQA/app/index.tsx
+++ SMQA/app/index.tsx
@@ -1,15 +1,23 @@
-import { Text, View } from "react-native";
+import { Text, View, StyleSheet } from 'react-native';
 
 export default function Index() {
-  return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
-    </View>
-  );
+	return (
+		<View style={styles.titleContainer}>
+			<Text style={styles.text}>
+				Edit app/index.tsx to edit this screen.
+			</Text>
+		</View>
+	);
 }
+
+const styles = StyleSheet.create({
+	titleContainer: {
+		flex: 1,
+		justifyContent: 'center',
+		alignItems: 'center',
+	},
+	text: {
+		fontSize: 20,
+		fontWeight: 'bold',
+	},
+});
